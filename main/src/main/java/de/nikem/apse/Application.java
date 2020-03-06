5f024bbc3fc0e79d9b7a67875fff0b4835df3e62
Andreas Knees <andreas.knees@nikem.de>
2020-03-06 16:24:42 +0100

    locale and timezone

--- main/src/main/java/de/nikem/apse/Application.java
+++ main/src/main/java/de/nikem/apse/Application.java
@@ -1,13 +1,21 @@
 package de.nikem.apse;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.Locale;
+import java.util.TimeZone;
+
+@Slf4j
 @SpringBootApplication
 @EnableScheduling
 public class Application {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.GERMANY);
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Berlin"));
+        log.info("Set locale {} and server timezone {}", Locale.getDefault(), TimeZone.getDefault());
         SpringApplication.run(Application.class, args);
     }
 }
